cb5f596307e02f08e515116945ca89d9b8446485
Michele Sciabarra <michele@sciabarra.com>
2020-07-26 18:49:24 +0200

    Backup Sun Jul 26 18:49:24 CEST 2020

--- backup.sh
+++ backup.sh
@@ -3,5 +3,8 @@ cd "$(dirname $0)"
 date
 docker exec -i wiki php /var/www/html/maintenance/sqlite.php --backup-to /var/www/backup/noiopen.sqlite
 cd backup
-split -b 999K --additional-suffix=.bin noiopen.sqlite noiopen.sqlite- 
- 
+split -b 99M --additional-suffix=.bin noiopen.sqlite noiopen.sqlite- 
+cd ..
+git add images/ backup/
+git commit -m "Backup $(date)" -a
+git push origin master
